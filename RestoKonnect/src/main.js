1995c2b2f049ea1468b645bc0b912d7a8a885a86
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2023-11-08 17:12:21 +0100

    fix error

--- RestoKonnect/src/main.js
+++ RestoKonnect/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import router from "./routes/route"
+import router from "../src/routes/route.js"
 const app = createApp(App)
 
 app.use(router)
