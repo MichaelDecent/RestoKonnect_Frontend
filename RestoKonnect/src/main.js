aba5557bfce5d56e1ffe32b2b016eaad9bd8e10f
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2023-11-10 01:18:20 +0100

    added fuctionalities

--- RestoKonnect/src/main.js
+++ RestoKonnect/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import './index.css'
-import router from "./routes/route.js"
+import router from "./routes/route"
 const app = createApp(App)
 
 app.use(router)
